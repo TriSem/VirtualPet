2b5c1f63a45ecbb3d49ec20085703f660fb72397
TriSem <tristan.semmler@gmx.de>
2021-02-06 20:50:57 +0100

    Update Trait.cs

--- Assets/Scripts/World/Trait.cs
+++ Assets/Scripts/World/Trait.cs
@@ -3,35 +3,5 @@
 [RequireComponent(typeof(WorldObject))]
 public abstract class Trait : MonoBehaviour
 {
-}
-
-public class Food : Trait
-{
-    [SerializeField] float maximumAmount = 100f;
-    public float RemainingAmount { get; private set; }
-
-
-    public void Fill(float amount)
-    {
-        RemainingAmount = Mathf.Min(maximumAmount, RemainingAmount + amount);
-    }
-
-    /// <summary>
-    /// Reduce amount of food left by up to the specified amount.
-    /// </summary>
-    /// <returns>Actual amount of consumed food.</returns>
-    public float Consume(float amount)
-    {
-        if (RemainingAmount >= amount)
-        {
-            RemainingAmount -= amount;
-            return amount;
-        }
-        else
-        {
-            float consumed = RemainingAmount;
-            RemainingAmount = 0;
-            return consumed;
-        }
-    }
+    public abstract uint TraitId { get; }
 }
