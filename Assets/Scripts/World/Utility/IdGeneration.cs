671cffa7d41fb5e5cf6c36a5872f9a7718c5b122
TriSem <tristan.semmler@gmx.de>
2021-02-06 12:32:15 +0100

    Update IdGeneration.cs

--- Assets/Scripts/World/Utility/IdGeneration.cs
+++ Assets/Scripts/World/Utility/IdGeneration.cs
@@ -1,12 +1,12 @@
 ﻿public static class IdGeneration
 {
     // String hashing algorithm by Daniel J. Bernstein.
-    public static ulong Djb2(string str)
+    public static uint Djb2(string str)
     {
-        ulong hash = 5381;
+        uint hash = 5381;
         foreach (char ch in str)
         {
-            ulong c = (byte)ch;
+            uint c = (byte)ch;
             hash = ((hash << 5) + hash) + c;
         }
         return hash;
