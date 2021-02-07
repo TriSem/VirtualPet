eae2ecf60235f6923f4b0b0a362b0ba3c01c3a6e
TriSem <tristan.semmler@gmx.de>
2021-02-07 12:22:36 +0100

    Created ActionSelection.cs

---
+++ Assets/Scripts/Actions/ActionSelection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSelection : MonoBehaviour
+{
+    List<PetAction> petActions = null;
+
+    void Start()
+    {
+        var actions = new List<PetAction>(GetComponents<PetAction>());    
+    }
+
+    void StartSelection()
+    {
+        var activeCandidates = new List<PetAction>();
+        foreach (var action in petActions)
+            if (action.RequirementsMet())
+                activeCandidates.Add(action);
+
+
+    }
+}
