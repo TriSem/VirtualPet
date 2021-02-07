1dc1d3bff3de7e66b123856932afb3b20adaf2d5
TriSem <tristan.semmler@gmx.de>
2021-02-07 12:21:52 +0100

    Created PetAgent.cs

---
+++ Assets/Scripts/PetAgent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PetAgent : MonoBehaviour
+{
+    [SerializeField] float tickRate = (1 / 10);
+    [SerializeField] MotorSystem motorSystem = null;
+
+    public NavMeshAgent NavAgent { get; private set; }
+    public DriveVector DriveVector { get; } = new DriveVector();
+
+    EventTree events = new EventTree();
+
+
+    List<Trait> percievedTraits = new List<Trait>();
+
+    public List<T> GetTraits<T>() where T : Trait
+    {
+        var selected = new List<T>();
+        foreach(var trait in percievedTraits)
+        {
+            if (trait is T t)
+                selected.Add(t);
+        }
+
+        return selected;
+    }
+
+    float lastTick = (1 / 10);
+
+
+    void Awake()
+    {
+        NavAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        if (lastTick + tickRate < time)
+        {
+            lastTick = time;
+        }
+    }
+
+    public bool EventActive(EventQuery query)
+    {
+        return events.EventActive(query);
+    }
+}
