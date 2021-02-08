796da2dfa7426b87cde94bfb00da82c2896e65d6
TriSem <tristan.semmler@gmx.de>
2021-02-08 12:11:01 +0100

    Created ActionObject.cs

---
+++ Assets/Scripts/Actions/ActionObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActionObject : MonoBehaviour
+{
+    [SerializeField] protected List<Outcome> outcomes = null;
+
+    public ActionStatus Status { get; protected set; }
+
+    public virtual bool IsUsable()
+    {
+        return Status == ActionStatus.Inactive;
+    }
+
+    public abstract void UseAction(PetAgent agent);
+    public abstract void Cancel();
+}
+
+public enum ActionStatus
+{
+    Inactive,
+    Ongoing,
+    Success,
+    Cancelled
+}
