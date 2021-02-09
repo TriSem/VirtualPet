5cf178c3600ebbb1af738a0c26ef254cbb03e13d
TriSem <tristan.semmler@gmx.de>
2021-02-09 06:40:51 +0100

    Import DispenseTreat.cs

---
+++ Assets/Scripts/Player/DispenseTreat.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DispenseTreat : MonoBehaviour
+{
+    [SerializeField] Transform treatPrefab = default;
+    Edible instance = null;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1) && instance == null)
+        {
+            instance = Instantiate(treatPrefab).GetComponent<Edible>();
+            instance.transform.SetParent(transform);
+            instance.transform.localPosition = Vector3.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            instance.transform.SetParent(null);
+            instance = null;
+        }
+    }
+}
