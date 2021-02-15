bc430e63d84b23f4898a5118854add988a9aa78f
TriSem <tristan.semmler@gmx.de>
2021-02-15 15:52:59 +0100

    Created TagTrigger.cs

---
+++ Assets/_Scripts/World/TagTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TagTrigger : MonoBehaviour
+{
+    [SerializeField] string tagName = default;
+
+    public bool Triggered { get; private set; }
+
+    void FixedUpdate()
+    {
+        Triggered = false;    
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(tagName))
+            Triggered = true;
+    }
+}
