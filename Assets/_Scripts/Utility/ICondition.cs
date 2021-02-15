24960f16b51b47df545640bb40d5e7ec4305506c
TriSem <tristan.semmler@gmx.de>
2021-02-15 15:53:12 +0100

    Created ICondition.cs

---
+++ Assets/_Scripts/Utility/ICondition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.AI;
+
+interface ICondition
+{
+    bool Met { get; }
+}
+
+class DestinationCondition : ICondition
+{
+    NavMeshAgent agent;
+
+    public DestinationCondition(NavMeshAgent agent)
+    {
+        this.agent = agent;
+    }
+
+    public bool Met => agent.remainingDistance <= agent.stoppingDistance;
+}
+
+public class InteractionCondition : ICondition
+{
+    Interaction interaction;
+
+    public InteractionCondition(Interaction interaction)
+    {
+        this.interaction = interaction;
+    }
+
+    public bool Met => interaction.PetInRange;
+}
