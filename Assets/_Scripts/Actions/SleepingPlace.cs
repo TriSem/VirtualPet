d031551fb587a9c615223360399fab92b2e38980
TriSem <tristan.semmler@gmx.de>
2021-02-15 15:52:15 +0100

    Added SleepingPlace.cs

---
+++ Assets/_Scripts/Actions/SleepingPlace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepingPlace : ActionObject
+{
+    public override void Cancel()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UseAction(PetAgent agent)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
