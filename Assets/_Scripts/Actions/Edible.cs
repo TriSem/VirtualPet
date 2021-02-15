216643236aeaf66bc008eb78a575632dbedb2a23
TriSem <tristan.semmler@gmx.de>
2021-02-15 15:51:50 +0100

    Update Edible.cs

--- Assets/_Scripts/Actions/Edible.cs
+++ Assets/_Scripts/Actions/Edible.cs
@@ -1,45 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Edible : ActionObject
 {
     PetAgent agent = null;
+
+    [SerializeField, Range(0f, 1000f)] 
+    float fillValue = 5f;
 
     [SerializeField] bool refillable = false;
     [SerializeField] Interaction interaction = default;
 
     public override void Cancel()
     {
-        Status = ActionStatus.Cancelled;
+        Status = ActionStatus.Inactive;
     }
 
     public override void UseAction(PetAgent agent)
     {
-        this.agent = agent;  
+        this.agent = agent;
+        agent.Motor.GoInteract(interaction);
         Status = ActionStatus.Ongoing;
     }
 
     void GetEaten()
     {
-
-    }
-
-    void Start()
-    {
-        
+        agent.Stomach.ChangeFill(fillValue);
+        Cancel();
+        gameObject.transform.position = new Vector3(1000, 1000, 1000);
     }
 
     void Update()
     {
         if(Status == ActionStatus.Ongoing)
         {
-            float zDistanceToPivot = agent.BoundingBox.bounds.extents.z;
-            var pointOnBounds = agent.transform.position + agent.transform.forward * zDistanceToPivot;
-            if(Vector3.Distance(pointOnBounds, transform.position) < 1f)
-            {
-                this.gameObject.SetActive(false);
-            }
+            if (interaction.PetInRange)
+                GetEaten();
         }
     }
 }
