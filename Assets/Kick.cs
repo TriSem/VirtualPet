95624c22cd36667ea3ce168f751e2259f1e46962
TriSem <tristan.semmler@gmx.de>
2021-02-17 17:56:24 +0100

    Added player ability to kick around things

---
+++ Assets/Kick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Kick : MonoBehaviour
+{
+    [SerializeField] float strength = 1f;
+
+    void OnTriggerEnter(Collider other)
+    { 
+        if(other.attachedRigidbody != null)
+        {
+            var direction = other.transform.position - transform.position;
+            direction.y = 1f;
+            direction.Normalize();
+            other.attachedRigidbody.AddForce(direction * strength);
+        }
+    }
+}
