574f290009e6fdffe687c8d34e3306f2866267e2
TriSem <tristan.semmler@gmx.de>
2021-02-17 17:55:53 +0100

    Added reorienting hand for petting

---
+++ Assets/_Scripts/Player/Hand.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField] Transform cam = null;
+
+    void Update()
+    {
+        var ray = new Ray(cam.position, cam.forward);
+        Debug.DrawRay(ray.origin, ray.direction + ray.origin);
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            if(Physics.Raycast(ray, out RaycastHit info, 5f, 1 << 12))
+            {
+                transform.position = info.point;
+                transform.up = info.normal;
+            }
+        }
+    }
+}
