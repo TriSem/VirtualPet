6778102e757278d7a86dce2aca22336684438119
TriSem <tristan.semmler@gmx.de>
2021-03-04 14:39:18 +0100

    Added Learning.cs

---
+++ Assets/_Scripts/Actions/Learning.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+    Dictionary<string, LearnedSkill> skills = new Dictionary<string, LearnedSkill>();
+}
+
+[Serializable]
+public class LearnedSkill
+{
+    [SerializeField] string actionName = "";
+    Dictionary<string, int> phrasesHeard = new Dictionary<string, int>();
+
+    ICondition startTrigger, endTrigger;
+
+    public bool Active { get; set; } = false;
+    public bool Learned { get; set; } = false;
+}
