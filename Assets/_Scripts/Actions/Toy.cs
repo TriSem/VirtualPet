cc04e87f3e7c95b4c0f859ceeceddc29338ea96c
TriSem <tristan.semmler@gmx.de>
2021-03-04 14:38:34 +0100

    Renamed Play.cs to Toy.cs

---
+++ Assets/_Scripts/Actions/Toy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Toy : ActionObject
+{
+    [SerializeField] Interaction interaction = null;
+    new Rigidbody rigidbody = null;
+    new Collider collider = null;
+    PetAgent agent = null;
+
+    public override void Cancel()
+    {
+    }
+
+    public override void UseAction(PetAgent agent)
+    {
+        Status = ActionStatus.Ongoing;
+        Pursue();
+    }
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetPhysicsEnabled(bool enabled)
+    {
+        if(enabled)
+        {
+            collider.enabled = true;
+            rigidbody.isKinematic = false;
+        }
+        else
+        {
+            collider.enabled = false;
+            rigidbody.isKinematic = true;
+        }
+    }
+
+    public override bool IsUsable()
+    {
+        return rigidbody.velocity.magnitude < 1f;
+    }
+
+    void Pursue()
+    {
+        agent.Motor.Pursue(transform, 1f);
+    }
+
+    bool CanGrabBall()
+    {
+        return interaction.PetInRange;
+    }
+
+    void Grab()
+    {
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+        transform.parent = agent.Snoot.transform;
+        transform.localPosition = agent.Snoot.transform.position;
+    }
+
+    void Throw()
+    {
+        transform.parent = null;
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
+        rigidbody.velocity = agent.transform.forward + Vector3.up;
+    }
+}
+
+public class PursuitState : PetState
+{
+    MotorSystem pursuer;
+    Transform target;
+
+    public PursuitState(List<Transition> transitions, MotorSystem pursuer, Transform target) : base(transitions)
+    {
+        this.pursuer = pursuer;
+        this.target = target;
+    }
+
+    public override void OnExit()
+    {
+        pursuer.Stop();
+    }
+
+    public override void Update()
+    {
+        pursuer.MoveTo(target.position + target.forward);
+    }
+}
