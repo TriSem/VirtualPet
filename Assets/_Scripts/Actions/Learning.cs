2f721d7afa26f05effbc769be431517bf3bb28da
TriSem <tristan.semmler@gmx.de>
2021-03-24 16:54:18 +0100

    Update Learning.cs

--- Assets/_Scripts/Actions/Learning.cs
+++ Assets/_Scripts/Actions/Learning.cs
@@ -1,20 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Learning : MonoBehaviour
 {
-    Dictionary<string, LearnedSkill> skills = new Dictionary<string, LearnedSkill>();
+    [Tooltip("Actions that can be associated with a command.")]
+    [SerializeField] List<string> actionNames = null;
+    Dictionary<string, >
+    bool learning = false;
+    string currentlyLearning = "";
+
+    void Start()
+    {
+        foreach(var name in actionNames)
+        {
+            phrases.Add(new Phrase(name));
+        }
+    }
+
+    public void PhraseHeard(string phrase)
+    {
+        if (learning)
+        {
+
+        }
+        else
+        {
+            NotifyActionSelection();
+        }
+    }
+
+    void NotifyActionSelection()
+    {
+
+    }
 }
 
-[Serializable]
-public class LearnedSkill
+public class Phrase
 {
-    [SerializeField] string actionName = "";
-    Dictionary<string, int> phrasesHeard = new Dictionary<string, int>();
+    string name;
+    Dictionary<string, int> actionToCount;
 
-    ICondition startTrigger, endTrigger;
+    public Phrase(string name)
+    {
+        this.name = name;
+    }
 
-    public bool Active { get; set; } = false;
-    public bool Learned { get; set; } = false;
+    public string LikeliestAction()
+    {
+        float total = TotalTimesRegistered();
+        float highestMatch = 0f;
+        foreach(var entry in actionToCount)
+        {
+            float likelihood = (float)entry.Value / total;
+        }
+        return null;
+    }
+
+    public int TotalTimesRegistered()
+    {
+        int result = 0;
+        foreach (var element in actionToCount)
+            result += element.Value;
+        return result;
+    }
 }
